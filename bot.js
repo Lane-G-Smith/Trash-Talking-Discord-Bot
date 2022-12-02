291a783bcedafcab8eec9b9bbadc021894d4428d
Lane <highsierrasmith1@gmail.com>
2022-12-02 16:18:35 -0700

    Modified with new key

--- bot.js
+++ bot.js
@@ -1,21 +1,31 @@
-require('dotenv').config();
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
+// hide key token in .env file . Won't upload to github
+require('dotenv').config()
+// this installs the packages and configures permissions(intents)
+const { Client, GatewayIntentBits} = require('discord.js');
+const client = new Client ({intents: [GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping, 
+    GatewayIntentBits.GuildScheduledEvents
+]});
+// confirm login in console, send intro message to Discord
+// log in with token from .env file
 client.on('ready', () => {
-    console.log("logged in and ready to blow y'all");
-})
+    console.log(`Logged in as ${client.user.tag}!`),
+    message.channel.send("What up y'all")
+});
+// create key for speaking to bot
+prefix = ('!');
+// text responses to specific words or phrases typed in Discord
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "Bully-Bot"))
+    { message.channel.send("wut?");
+    };
+    if (message.content.startsWith(prefix + "testing"))
+    {message.channel.send("1,2,3")
+    };
+});
 
-// client.on('message', msg => {
-//     if (msg.content === int) {
-//         msg.reply("you know how to type numbers, good for you asshole. So can I, 69 69 69 69 get fucked");
-//     }
-// })
-
-client.login(process.env.TOKEN);
-
-//make the bot respond to a specific word
-if(message.content.toLowerCase().includes(` ${your_variable} `)) {
-    // your code
-  }
+client.login(process.env.TOKEN)
