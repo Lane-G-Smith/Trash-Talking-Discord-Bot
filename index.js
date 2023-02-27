7fd0798b6d1802e7c0bb5de873ab7040b91bbb13
Lane G Smith <lanegphotography@gmail.com>
2023-02-27 13:44:31 -0700

    Updated Apis add kanye quotes add trump quotes add useless fact

--- index.js
+++ index.js
@@ -20,21 +20,14 @@ const client = new Client({intents:
 
 // log in with token from .env file
 client.on('ready', () => {
-    console.log(`Main page works! Logged in as ${client.user.tag}!`)
+    console.log(`Fuck You!! Logged in as ${client.user.tag}!`)
 });
-
-// reply to ping with pong
-client.on('messageCreate', message => {
-	if (message.content === 'ping') {
-		message.reply('pong');
-	}
-	});
-
 
 // call sweet burn api
 client.on('messageCreate', message => {
-	if (message.content === 'dumb-insult') {
-		dumbInsult().then(data => message.reply(data.insult));
+	if (message.content.toLowerCase() === 'dumb-insult') {
+		dumbInsult()
+		.then(data => message.reply(data.insult));
 	}
 });
 async function dumbInsult() {
@@ -45,8 +38,9 @@ async function dumbInsult() {
 
 // call mom joke api
 client.on('messageCreate', message => {
-	if (message.content.includes('mom')) {
-		momJoke().then(data => message.reply(data.joke));
+	if (message.content.toLowerCase().includes('mom')) {
+		momJoke()
+		.then(data => message.reply(data.joke));
 	}
 });
 async function momJoke() {
@@ -54,6 +48,70 @@ async function momJoke() {
 	let data = await response.json()
 	return data;
 }
+
+// call Chuck Norris Joke api
+client.on('messageCreate', message => {
+	if (message.content.toLowerCase().includes('chuck')) {
+		chuckNorris()
+		.then(data => message.reply(data.value));
+	}
+});
+async function chuckNorris() {
+	let response =  await fetch('https://api.chucknorris.io/jokes/random/');
+	let data = await response.json()
+	return data;
+}
+
+// call Kanye quote api
+client.on('messageCreate', message => {
+	if (message.content.toLowerCase().includes('kanye')) {
+		kanye()
+		.then(data => message.reply(data.quote + "...............K a n y e  W e s t"));
+	}
+});
+async function kanye() {
+	let response =  await fetch('https://api.kanye.rest/');
+	let data = await response.json()
+	return data;
+}
+
+// call Trump quote api
+client.on('messageCreate', message => {
+	if (message.content.toLowerCase().includes('idiot')) {
+		trump()
+		.then(data => message.reply((data.value) + "............... D o n a l d  T r u m p"));
+	}
+});
+async function trump() {
+	let response =  await fetch('https://tronalddump.io/random/quote');
+	let data = await response.json()
+	return data;
+}
+
+// call useless fact api
+client.on('messageCreate', message => {
+	if (message.content.toLowerCase().includes('useless')) {
+		useless()
+		.then(data => message.reply(data.text));
+	}
+});
+async function useless() {
+	let response =  await fetch('https://uselessfacts.jsph.pl/random.json?language=en');
+	let data = await response.json()
+	return data;
+}
+
+// // call Giphy api
+// client.on('messageCreate', message => {
+// 	if (message.content.includes('balls')) {
+// 		termMeme().then(data => message.reply(data[0].images.original.url));
+// 	}
+// });
+// async function termMeme() {
+// 	let response =  await fetch('https://api.giphy.com/v1/gifs/translate?api_key=4U0KfRgkrwJNDx0OHiP3xWhK6jGuklUy&s=balls');
+// 	let data = await response.json()
+// 	return data;
+// }
 
 // client.login
 client.login(process.env.TOKEN)
